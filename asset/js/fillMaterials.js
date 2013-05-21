763c0db0303dbebae2715977f9c06c544f3ba568
astrider.com@gmail.com <astrider.com@gmail.com>
2013-05-21 14:52:02 +0900

    fixed total css

--- asset/js/fillMaterials.js
+++ asset/js/fillMaterials.js
@@ -9,8 +9,7 @@ if(!window.fillMaterials) fillMaterials = {};
 			if(values.length != colors.length) return;
 
 			for(var i=0; i<keys.length; i++) {
-				if(values[i] < 0.5) $(keys[i]).addClass("material-" + colors[i] + "-dark");
-				// else if(values[i] < 0.8) $(keys[i]).addClass("material-" + colors[i] + "-normal");
+				if(values[i] < 0.7) $(keys[i]).addClass("material-" + colors[i] + "-dark");
 				else $(keys[i]).addClass("material-" + colors[i] + "-light");
 			}
 		}
